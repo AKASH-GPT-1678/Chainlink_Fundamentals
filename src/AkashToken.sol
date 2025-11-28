82965050604389a0478c4dd929589485be978ad2
AKASH-GPT-1678 <akashgpt5974@gmail.com>
2025-11-28 22:04:03 +0530

    akashtgoken g

---
+++ src/AkashToken.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { AccessControl } from "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract AkashToken is ERC20, AccessControl {
+       bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    constructor() ERC20("Akash Token", "AKT") {}
+
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
+    }
+}
