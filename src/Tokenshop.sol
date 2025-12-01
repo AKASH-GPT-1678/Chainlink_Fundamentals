7f18e9146ba693717751d63344cfa0e0c59fab70
AKASH-GPT-1678 <akashgpt5974@gmail.com>
2025-12-01 14:18:55 +0530

    chian lik

--- src/Tokenshop.sol
+++ src/Tokenshop.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
+//lib\chainlink-evm\contracts\src\v0.8\shared\interfaces\AggregatorV3Interface.sol
 
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
-
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract TokenShop {}
